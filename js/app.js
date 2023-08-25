f94e69003635467575192188c5d55b50a1138757
Your Name <henokmicrometre@gmail.com>
2023-08-25 18:28:52 +0000

    added static

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+const logger = require('morgan')
+
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+
+app.get('/video', (req, res) => {
+  res.sendFile('alprVideo.mp4', { root: 'public'});
+});
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
@@ -19,6 +35,9 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+
+
+
+server.listen(5000, () => {
+  console.log('listening on *:5000');
 });
